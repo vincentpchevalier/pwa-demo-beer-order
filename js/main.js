642d23f2f3f632841c927b408700d22ecd9345ac
vincentpchevalier <vincentpchevalier@proton.me>
2024-06-12 09:38:08 -0400

    Create addListeners function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import { openCache } from './cache.js';
 
 let datacache = null;
-let isOnline = 'online' in navigator && navigator.onLine;
+let SW = null;
+let isOnline = 'onLine' in navigator && navigator.onLine;
 
 function init() {
 	console.log('App initialized');
@@ -14,24 +15,71 @@ function init() {
 
 	registerServiceWorker();
 	addListeners();
+	console.log('online' in navigator, navigator.onLine);
+	showContent();
 }
 
 function registerServiceWorker() {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker
 			.register('./sw.js')
-			.then(() => {
+			.then((registration) => {
+				SW =
+					registration.installing ||
+					registration.waiting ||
+					registration.active;
 				console.log('Service worker registered');
 			})
 			.catch((error) => {
 				console.log('Service worker registration failed', error);
 			});
+
+		if (navigator.serviceWorker.controller) {
+			console.log('We have a service walker installed.');
+		}
 	} else {
 		console.log('Service workers not supported');
 	}
 }
 
-function addListeners() {}
+function addListeners() {
+	window.addEventListener('online', () => {
+		console.log('Online now!');
+		isOnline = true;
+	});
+
+	window.addEventListener('offline', () => {
+		console.log('Oops, offline!');
+		isOnline = false;
+	});
+
+	document.getElementById('go-to-orders').addEventListener('click', () => {
+		console.log('Navigating to orders page');
+	});
+
+	document.getElementById('content').addEventListener('click', (event) => {
+		console.log(event.target);
+	});
+}
+
+function showContent() {
+	console.log(isOnline);
+	if (isOnline) {
+		document.getElementById('content').innerHTML = `
+      <h2>Beers</h2>
+      <div class="beers"></div>
+    `;
+	}
+
+	if (!isOnline) {
+		document.getElementById('content').innerHTML = `
+      <h2>404</h2>
+      <p>Currently offline. Please check your connection and refresh.</p>
+      <button id="refresh">Refresh</button>
+      <p id="see-orders">See orders</p>
+    `;
+	}
+}
 
 document.addEventListener('DOMContentLoaded', init);
 
